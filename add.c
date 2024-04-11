7fa094ab3f82d21dfc538c5097645398540ed633
小米-潘爽 <56994455+panshuang3@users.noreply.github.com>
2024-04-12 00:25:49 +0800

    更新 add.c
    添加二分查找算法

--- add.c
+++ add.c
@@ -4,3 +4,18 @@ int Add(int a, int b)
 	return a + b;
 }
 
+int BinarySearch(int *nums, int length, int target)
+{
+    int left = 0, right = length - 1, mid = 0;
+    while (left <= right) {
+        mid = (left + right) / 2;
+        if (target > nums [mid]) {
+            left = mid + 1;
+        } else if (target < nums [mid]) {
+            right = mid - 1;
+        } else {
+            return mid;
+        }
+    }
+    return -1;
+}
